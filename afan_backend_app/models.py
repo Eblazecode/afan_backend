dee83961bc9b8960e9fd46f48a89169b71fd8ee1
Eblazecode <dynablaze@gmail.com>
2025-08-18 08:21:52 +0100

    fixed the register added member id to user

--- afan_backend_app/models.py
+++ afan_backend_app/models.py
@@ -98,6 +98,14 @@ class KYCSubmission(models.Model):
     def get_membership_id(self):
         return self.membership_id if self.membership_id else "Not Assigned"
     # -*- coding: utf-8 -*-
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    member_id = models.CharField(max_length=50, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
 # payments model
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
