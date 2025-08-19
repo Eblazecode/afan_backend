d10c38596eddf5e8c198ee066bde9a86dab5ea6b
Eblazecode <dynablaze@gmail.com>
2025-08-19 05:16:02 +0100

    reversed  migration files for fixed AFAN ID from sign up CHNAGES

--- afan_backend_app/models.py
+++ afan_backend_app/models.py
@@ -114,7 +114,6 @@ class KYCSubmission(models.Model):
         ('Other', 'Other'),
     ]
 
-    user = models.OneToOneField(CustomUser , on_delete=models.CASCADE, default="")
     firstName = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
     phoneNumber = models.CharField(max_length=11)
@@ -147,16 +146,7 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} Profile"
 
-# payments model
-class Payment(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    payment_date = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Completed', 'Completed'), ('Failed', 'Failed')], default='Pending')
-    transaction_id = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return f"Payment {self.transaction_id} - {self.user.username} - {self.amount} - {self.payment_status}"
+#
 
 # fetch membership_id
 
