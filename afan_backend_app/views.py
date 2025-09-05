d64a40e5748f1d0a5aadad7cdcd6c248b87edb51
Eblazecode <dynablaze@gmail.com>
2025-09-05 13:27:12 +0100

    REVAMP AGEN LOGIN VIEW for agent id  to email 3

--- afan_backend_app/views.py
+++ afan_backend_app/views.py
@@ -221,9 +221,7 @@ def register_agent(request):
     }, status=201)
 
 
-# Configure logging
-@api_view(['POST'])
-@permission_classes([AllowAny])
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
