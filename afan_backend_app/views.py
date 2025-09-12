971643e47aa5ff6f8bfc91054e40e84f78a21751
Eblazecode <dynablaze@gmail.com>
2025-09-12 16:12:07 +0100

    fixing agent payment and reciept 9

--- afan_backend_app/views.py
+++ afan_backend_app/views.py
@@ -875,18 +875,17 @@ from rest_framework.decorators import api_view
 from django.conf import settings
 
 @api_view(["POST"])
-@permission_classes([AllowAny])
 def initiate_agent_payment(request):
     membership_id = request.data.get("membership_id")
-    email = request.data.get("email")
+    phonenumber = request.data.get("phonenumber")
     amount = request.data.get("amount")  # pass in kobo, e.g. 5000 NGN = 500000
 
-    if not membership_id or not email or not amount:
+    if not membership_id or not phonenumber or not amount:
         return Response({"status": "error", "message": "Missing fields"}, status=400)
 
     headers = {"Authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}"}
     payload = {
-        "email": email,
+        "phonenumber": phonenumber,
         "amount": amount,
         "callback_url": "https://www.afannigeria.com/agent-payment-callback",
         "metadata": {
