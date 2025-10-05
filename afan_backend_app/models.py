22d9bcfc7b2726ecbbcc70fb3d9d2ac837ba197e
Eblazecode <dynablaze@gmail.com>
2025-10-05 19:04:31 +0100

    fixing new fields on the KYC tables

--- afan_backend_app/models.py
+++ afan_backend_app/models.py
@@ -156,7 +156,7 @@ class KYCSubmission(models.Model):
     farmSize = models.DecimalField(max_digits=10, decimal_places=2)
     yearsOfExperience = models.PositiveIntegerField()
     primaryCrops = models.CharField(max_length=255)
-    secondaryCrops = models.CharField(max_length=255, default="secondary crops")
+    secondaryCrops = models.CharField(max_length=255, blank=True, null=True)
     farmLocation = models.TextField()
     passportPhoto = models.URLField(max_length=500, blank=True, null=True)
     submittedAt = models.DateTimeField(auto_now_add=True)
