631640533eb49401a9530f41bbf2e5d66def237c
Eblazecode <dynablaze@gmail.com>
2025-11-06 06:48:47 +0100

    FIXING AGENT DASHBOARD 6

--- afan_backend_app/views.py
+++ afan_backend_app/views.py
@@ -2252,7 +2252,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
-from .models import Agent, Member, KYCSubmission
 import re, random
 
 
